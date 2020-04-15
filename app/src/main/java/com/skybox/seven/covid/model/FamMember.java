b55e3167f9ca643457cb1b3c9fa0deaa1e186588
Steve <stevechikwiri@gmail.com>
2020-04-15 19:14:11 +0200

    member addition

---
+++ app/src/main/java/com/skybox/seven/covid/model/FamMember.java
@@ -0,0 +1,37 @@
+package com.skybox.seven.covid.model;
+
+public class FamMember {
+
+    private String famMemberID, name, phoneNumber;
+    private String userID; //to whom they are attached
+    boolean verified = false;
+
+    public FamMember(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFamMemberID() {
+        return famMemberID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
--- app/src/main/java/com/skybox/seven/covid/ui/ContactActivity.java
+++ app/src/main/java/com/skybox/seven/covid/ui/ContactActivity.java
@@ -3,28 +3,64 @@ package com.skybox.seven.covid.ui;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.skybox.seven.covid.R;
+import com.skybox.seven.covid.model.FamMember;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactActivity extends AppCompatActivity {
+
+    LayoutInflater inflater;
+    LinearLayout membersView;
+    ArrayList<FamMember> members = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
+        membersView = findViewById(R.id.membersParent);
         RelativeLayout addPersonView = findViewById(R.id.addPerson);
-        LayoutInflater inflater = LayoutInflater.from(this);
+        inflater = LayoutInflater.from(this);
 
         addPersonView.setOnClickListener(v -> {
             BottomSheetDialog bottomDialog = new BottomSheetDialog(this);
             View view = inflater.inflate(R.layout.person_dialog,null);
+
+            ((Button)view.findViewById(R.id.saveMember)).setOnClickListener(vw->{
+                String name = String.valueOf(((EditText)view.findViewById(R.id.userName)).getText());
+                String phoneNumber = String.valueOf(((EditText)view.findViewById(R.id.phoneNumber)).getText());
+                FamMember famMember = new FamMember(name,phoneNumber);
+                members.add(famMember);
+                setupFamily();
+                bottomDialog.dismiss();
+            });
+
             bottomDialog.setContentView(view);
             bottomDialog.show();
         });
+
+        FamMember member = new FamMember("Mijiga", "0998530227");
+        members.add(member);
+
+        setupFamily();
+    }
+
+    public void setupFamily() {
+        membersView.removeAllViews();
+        for (FamMember member : members) {
+            View view = inflater.inflate(R.layout.member_item,null);
+            ((TextView)view.findViewById(R.id.name)).setText(member.getName());
+            membersView.addView(view);
+        }
     }
 }
