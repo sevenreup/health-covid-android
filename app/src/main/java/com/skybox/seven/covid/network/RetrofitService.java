2dddec6d0634534b867a581d47f88f0dfad95f45
Steve <stevechikwiri@gmail.com>
2020-04-17 02:06:06 +0200

    contact addiot post

--- app/src/main/java/com/skybox/seven/covid/network/RetrofitService.java
+++ app/src/main/java/com/skybox/seven/covid/network/RetrofitService.java
@@ -1,6 +1,10 @@
 package com.skybox.seven.covid.network;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.skybox.seven.covid.model.FamMember;
 import com.skybox.seven.covid.network.responses.LoginResponse;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
@@ -17,4 +21,7 @@ public interface RetrofitService {
     @FormUrlEncoded
     @POST("")
     Call<String> register();
+    @FormUrlEncoded
+    @POST("contact/add")
+    Call<String> saveContacts(@Field("contacts") ArrayList<FamMember> contacts, @Field("location") LatLng latLng);
 }
