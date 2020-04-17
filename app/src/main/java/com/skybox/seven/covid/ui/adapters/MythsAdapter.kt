0060432b435a58ca6f12a96013cd15dbda25f333
Kells Works SoftTech <56794404+KellsWorks@users.noreply.github.com>
2020-04-17 03:24:34 +0200

    Update

--- app/src/main/java/com/skybox/seven/covid/ui/adapters/MythsAdapter.kt
+++ app/src/main/java/com/skybox/seven/covid/ui/adapters/MythsAdapter.kt
@@ -3,7 +3,9 @@ package com.skybox.seven.covid.ui.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.skybox.seven.covid.R
 import com.skybox.seven.covid.ui.adapters.MythsModel
 import com.skybox.seven.covid.ui.fragment.main.MythBusterFragment
@@ -31,9 +33,21 @@ class MythsAdapter(val arrayList: ArrayList<MythsModel>, val context: MythBuster
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindItems(arrayList[position])
+
         holder.itemView.setOnClickListener {
+            when(position){
+                0 -> {
+                    val dialog = BottomSheetDialog(context.activity!!)
+                    dialog.setTitle("MythBusters")
+                    dialog.show()
+                    Toast.makeText(context.activity, "What is COVID19?", Toast.LENGTH_LONG ).show()
+                }
+            }
 
         }
+    }
+    interface onItemClickListener{
+
     }
 
 
