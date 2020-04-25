413911b08ce88953180adfd37985a0628f7093a3
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-04-26 00:14:10 +0200

    started adding personal info

--- app/src/main/java/com/skybox/seven/covid/ui/fragment/main/PersonalFragment.java
+++ app/src/main/java/com/skybox/seven/covid/ui/fragment/main/PersonalFragment.java
@@ -3,18 +3,23 @@ package com.skybox.seven.covid.ui.fragment.main;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.skybox.seven.covid.R;
+import com.skybox.seven.covid.viewmodels.LocationViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PersonalFragment extends Fragment {
-
+    private LocationViewModel locationViewModel;
+    private TextView geoFenceText;
     public PersonalFragment() {
         // Required empty public constructor
     }
@@ -24,6 +29,18 @@ public class PersonalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal, container, false);
+        View v = inflater.inflate(R.layout.fragment_personal, container, false);
+        geoFenceText = v.findViewById(R.id.geofence);
+
+        locationViewModel = new ViewModelProvider(getActivity(), new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication())).get(LocationViewModel.class);
+
+        locationViewModel.currentGeofence.observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                geoFenceText.setText(s);
+            }
+        });
+
+        return v;
     }
 }
