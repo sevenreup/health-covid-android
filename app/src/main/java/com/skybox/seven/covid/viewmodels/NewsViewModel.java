8affd661597ef7395b05c256e190d7bb7d930208
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-05-24 13:02:42 +0200

    moved from unirest to retrofit

--- app/src/main/java/com/skybox/seven/covid/viewmodels/NewsViewModel.java
+++ app/src/main/java/com/skybox/seven/covid/viewmodels/NewsViewModel.java
@@ -1,88 +1,59 @@
 package com.skybox.seven.covid.viewmodels;
 
 import android.app.Application;
-import android.os.AsyncTask;
-import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
-import com.google.gson.Gson;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import com.skybox.seven.covid.model.NewsArticle;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.skybox.seven.covid.network.NewsFactory;
+import com.skybox.seven.covid.network.NewsService;
+import com.skybox.seven.covid.network.responses.NewsResponse;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class NewsViewModel extends AndroidViewModel {
     public MutableLiveData<Boolean> networkErrors = new MutableLiveData<>();
     public MutableLiveData<Boolean> loading = new MutableLiveData<>();
     public MutableLiveData<List<NewsArticle>> newsArticles = new MutableLiveData<>(new ArrayList<>());
+    private Retrofit retrofit;
+    private static final String API_KEY = "b3d54181b14e4d9faaa3be9909a1953f", TYPE = "covid", FROM = "2020-04-24=", TO ="2020-05-27", SORT = "popularity";
+
     public NewsViewModel(@NonNull Application application) {
         super(application);
+        retrofit = NewsFactory.getRetrofit(application);
     }
-public void getNews() {
-        new NewsCall().execute();
-}
-    private class NewsCall extends AsyncTask<String, Void, String> {
 
-        HttpResponse<String> response;
+    public void getNews() {
+        loading.postValue(true);
 
-        @Override
-        protected String doInBackground(String... strings) {
-            loading.postValue(true);
-            try {
-                response = Unirest.get("https://newsapi.org/v2/everything?q=covid&qInTitle=covid&pageSize=10&from=2020-04-24&to=2020-05-27&sortBy=popularity&apiKey=b3d54181b14e4d9faaa3be9909a1953f").asString();
-            } catch (UnirestException e) {
+        NewsService service = retrofit.create(NewsService.class);
+        Call<NewsResponse> responseCall = service.getNews(TYPE, TYPE, 10, FROM, TO, SORT, API_KEY);
+        responseCall.enqueue(new Callback<NewsResponse>() {
+            @Override
+            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+                loading.postValue(false);
+                if (response.isSuccessful()) {
+                    networkErrors.postValue(false);
+
+                    newsArticles.setValue(response.body().getArticles());
+                } else {
+                    networkErrors.postValue(true);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<NewsResponse> call, Throwable t) {
                 loading.postValue(false);
                 networkErrors.postValue(true);
-                e.printStackTrace();
             }
-
-            if (response != null) {
-                return response.getBody();
-            } else {
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String response) {
-            super.onPostExecute(response);
-
-            if (response != null) {
-                networkErrors.postValue(false);
-                ArrayList<NewsArticle> list = new ArrayList();
-                Gson gson = new Gson();
-
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    JSONArray jsonArray = jsonObject.getJSONArray("articles");
-
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        NewsArticle article = gson.fromJson(jsonArray.getJSONObject(i).toString(), NewsArticle.class);
-                        list.add(article);
-                        System.out.println(article.getAuthor());
-                    }
-
-                    newsArticles.postValue(list);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                System.out.println(response);
-            } else {
-                networkErrors.postValue(true);
-            }
-            loading.postValue(false);
-        }
+        });
     }
-
 }
