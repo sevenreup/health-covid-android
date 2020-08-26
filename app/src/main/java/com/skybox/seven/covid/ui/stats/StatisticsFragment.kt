e5102879c419b0e752d178ce6c0b7132499ac9c5
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-08-26 15:11:41 +0200

    py edits

--- app/src/main/java/com/skybox/seven/covid/ui/stats/StatisticsFragment.kt
+++ app/src/main/java/com/skybox/seven/covid/ui/stats/StatisticsFragment.kt
@@ -6,64 +6,71 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.skybox.seven.covid.R
+import com.skybox.seven.covid.databinding.FragmentStatsBinding
 
 
 @Suppress("DEPRECATION")
 class StatisticsFragment : Fragment() {
+    private lateinit var binding: FragmentStatsBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-        view?.findViewById<TextView>(R.id.mw_situation)?.setOnClickListener {
+        binding = FragmentStatsBinding.inflate(inflater, container, false)
+        binding.mwContainer.setOnClickListener {
             findNavController().navigate(R.id.action_statsFragment_to_statsBarChatFragment)
             this.requireActivity().overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         }
-        view?.findViewById<TextView>(R.id.view_countries)?.setOnClickListener {
+        binding.viewCountries.setOnClickListener {
             findNavController().navigate(R.id.action_statsFragment_to_countriesFragment)
         }
-
-        return inflater.inflate(R.layout.fragment_stats, container, false)
-
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
         worldChat()
+        return binding.root
+
     }
 
-    private fun worldChat(){
-        val colors:ArrayList<Int> = ArrayList()
-        colors.add(this.resources.getColor(R.color.reply_orange_400))
-        colors.add(this.resources.getColor(R.color.reply_green_100))
-        colors.add(this.resources.getColor(R.color.reply_red_400))
+    private fun worldChat() {
+        val barDataSet = PieDataSet(
+                listOf(
+                        PieEntry(50F, getString(R.string.deaths)),
+                        PieEntry(30F, getString(R.string.active)),
+                        PieEntry(20F, getString(R.string.recovered))
+                ),
+                "key"
+        )
 
-
-        val chart = view?.findViewById<PieChart>(R.id.worldChat)
-        val bars: ArrayList<PieEntry> = ArrayList()
-
-        bars.add(PieEntry(50F, ""))
-        bars.add(PieEntry(30F, ""))
-        bars.add(PieEntry(20F, ""))
-
-        val barDataSet = PieDataSet(bars, "Key")
-        barDataSet.colors = colors
+        barDataSet.colors = arrayListOf(
+                ContextCompat.getColor(requireContext(),R.color.reply_orange_400),
+                ContextCompat.getColor(requireContext(),R.color.reply_green_100),
+                ContextCompat.getColor(requireContext(),R.color.reply_red_400)
+        )
         barDataSet.valueTextColor = Color.WHITE
         barDataSet.valueTextSize = 16F
         val barData = PieData(barDataSet)
 
+        with(binding.worldChat) {
+            data = barData
+            legend.isEnabled = false
+            description = null
+            holeRadius = PIE_RADIUS
+            setHoleColor(ContextCompat.getColor(requireContext(), R.color.background_purple))
+            animateY(PIE_ANIMATION_DURATION, Easing.EaseInOutQuart)
+            invalidate()
+        }
+    }
 
-        chart?.data = barData
-        chart?.centerText = "Cases"
-        chart?.description?.isEnabled = false
-        chart?.animateY(2000)
+    companion object {
+        private const val TEXT_ANIMATION_DURATION = 1000L
+        private const val PIE_ANIMATION_DURATION = 1500
+        private const val PIE_RADIUS = 75f
     }
 
 }
