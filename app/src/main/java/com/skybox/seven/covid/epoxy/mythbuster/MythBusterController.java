86f792ba536a8d67fb12fc3cea1a679d98f05e86
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-09-09 19:26:18 +0200

    bunch of more deletes

--- app/src/main/java/com/skybox/seven/covid/epoxy/mythbuster/MythBusterController.java
+++ app/src/main/java/com/skybox/seven/covid/epoxy/mythbuster/MythBusterController.java
@@ -2,22 +2,31 @@ package com.skybox.seven.covid.epoxy.mythbuster;
 
 import com.airbnb.epoxy.AutoModel;
 import com.airbnb.epoxy.Typed2EpoxyController;
-import com.skybox.seven.covid.data.entities.Advice;
 import com.skybox.seven.covid.data.entities.Myth;
 import com.skybox.seven.covid.epoxy.generic.ListLoadingModel_;
-import com.skybox.seven.covid.epoxy.prevention.PreventionEpoxyModel_;
 
 import java.util.List;
 
 public class MythBusterController extends Typed2EpoxyController<Boolean, List<Myth>> {
     @AutoModel
     ListLoadingModel_ listLoadingModel_;
+    MythCallbacks callbacks;
+
+    public MythBusterController(MythCallbacks callbacks) {
+        this.callbacks = callbacks;
+    }
 
     @Override
     protected void buildModels(Boolean loading, List<Myth> mythbusters) {
         listLoadingModel_.addIf(loading, this);
         for (Myth mythbuster: mythbusters) {
-            new MythBusterEpoxyModel_().id(mythbuster.getTitle()).mythbuster(mythbuster).addTo(this);
+            new MythBusterEpoxyModel_().id(mythbuster.getTitle())
+                    .listener((model, parentView, clickedView, position) -> callbacks.onMythClicked(mythbuster))
+                    .mythbuster(mythbuster).addTo(this);
         }
+    }
+
+    public interface MythCallbacks {
+        void onMythClicked(Myth myth);
     }
 }
--- app/src/main/java/com/skybox/seven/covid/ui/mythbusters/MythBustersFragment.java
+++ app/src/main/java/com/skybox/seven/covid/ui/mythbusters/MythBustersFragment.java
@@ -2,6 +2,7 @@ package com.skybox.seven.covid.ui.mythbusters;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -11,10 +12,11 @@ import android.view.ViewGroup;
 
 import com.airbnb.epoxy.EpoxyRecyclerView;
 import com.skybox.seven.covid.R;
+import com.skybox.seven.covid.data.entities.Myth;
 import com.skybox.seven.covid.epoxy.mythbuster.MythBusterController;
 import com.skybox.seven.covid.util.SpaceItemDecorator;
 
-public class MythBustersFragment extends Fragment {
+public class MythBustersFragment extends Fragment implements MythBusterController.MythCallbacks {
 
     private MythBusterController controller;
     private MythViewModel viewModel;
@@ -23,6 +25,13 @@ public class MythBustersFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        controller = new MythBusterController(this);
+        viewModel.mythsList.observe(getViewLifecycleOwner(), mythbusterModel -> controller.setData(false,mythbusterModel));
+        viewModel.getAllMyths();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,15 +39,16 @@ public class MythBustersFragment extends Fragment {
         View v =inflater.inflate(R.layout.fragment_myth_busters, container, false);
         EpoxyRecyclerView recyclerView = v.findViewById(R.id.MythRecyclerView);
 
-        viewModel = new ViewModelProvider(getActivity()).get(MythViewModel.class);
-        controller = new MythBusterController();
+        viewModel = new ViewModelProvider(requireActivity()).get(MythViewModel.class);
 
         recyclerView.addItemDecoration(new SpaceItemDecorator(20, true, false));
         recyclerView.setController(controller);
 
-        viewModel.mythsList.observe(getViewLifecycleOwner(), mythbusterModel -> controller.setData(false,mythbusterModel));
-        viewModel.getAllMyths();
-
         return v;
+    }
+
+    @Override
+    public void onMythClicked(Myth myth) {
+
     }
 }
