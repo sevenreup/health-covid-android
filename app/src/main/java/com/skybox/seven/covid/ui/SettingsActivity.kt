b98dece4753e97c92e871848480f06ddb3362abf
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-10-08 20:35:07 +0200

    added about screen

--- app/src/main/java/com/skybox/seven/covid/ui/SettingsActivity.kt
+++ app/src/main/java/com/skybox/seven/covid/ui/SettingsActivity.kt
@@ -3,12 +3,22 @@ package com.skybox.seven.covid.ui
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.skybox.seven.covid.R
 import com.skybox.seven.covid.util.Constants
 import com.yariksoffice.lingver.Lingver
+import kotlin.math.log
 
 private const val TAG = "SettingsActivity"
 class SettingsActivity : AppCompatActivity() {
@@ -23,6 +33,14 @@ class SettingsActivity : AppCompatActivity() {
     }
 
 
+    override fun onSupportNavigateUp(): Boolean {
+        Log.e(TAG, "onSupportNavigateUp: ")
+        if (supportFragmentManager.popBackStackImmediate()) {
+            return true
+        }
+        this.onBackPressed()
+        return true
+    }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         val listener = SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences: SharedPreferences, key: String ->
@@ -43,6 +61,14 @@ class SettingsActivity : AppCompatActivity() {
             preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
         }
 
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            findPreference<Preference>("license")?.setOnPreferenceClickListener {
+                startActivity(Intent(context, OssLicensesMenuActivity::class.java))
+                true
+            }
+        }
+
         override fun onResume() {
             super.onResume()
             preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
@@ -58,5 +84,15 @@ class SettingsActivity : AppCompatActivity() {
             val i = Intent(requireContext(), HomeActivity::class.java)
             startActivity(i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK))
         }
+    }
+}
+
+class AboutUsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_about_us, container, false)
+        view.findViewById<TextView>(R.id.freepik).text = Html.fromHtml("Icons made by <a href=\"https://www.flaticon.com/authors/freepik\" title=\"Freepik\">Freepik</a> from <a href=\"https://www.flaticon.com/\" title=\"Flaticon\">www.flaticon.com</a>")
+        view.findViewById<TextView>(R.id.flaticon).text = Html.fromHtml("Vector from <a href=\"https://www.freepik.com/vectors/typography\">Freepik</a>")
+        return view
     }
 }
