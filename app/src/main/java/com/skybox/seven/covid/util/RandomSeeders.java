8f5b1329e3bea9930e22c6cf796ba4674f0fd8f7
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-10-09 21:10:43 +0200

    bunch of work

--- app/src/main/java/com/skybox/seven/covid/util/RandomSeeders.java
+++ app/src/main/java/com/skybox/seven/covid/util/RandomSeeders.java
@@ -36,20 +36,20 @@ public class RandomSeeders {
         adviceList.add(new Advice(1,Constants.ENGLISH,"Wash your hands", "Regularly and thoroughly clean your hands with an alcohol-based hand rub or wash them with soap and water.",
                 "Washing your hands with soap and water or using alcohol-based hand rub kills viruses that may be on your hands.", R.drawable.ic_wash_hands));
         adviceList.add(new Advice(2,Constants.ENGLISH,"Social distancing","Maintain at least 1 metre (3 feet) distance between yourself and others.",
-                "When someone coughs, sneezes, or speaks they spray small liquid droplets from their nose or mouth which may contain virus. If you are too close, you can breathe in the droplets, including the COVID-19 virus if the person has the disease.",
+                "When someone coughs, sneezes, or speaks they spray small liquid droplets from their nose or mouth which may contain ic_virus. If you are too close, you can breathe in the droplets, including the COVID-19 ic_virus if the person has the disease.",
                 R.drawable.ic_wash_hands));
         adviceList.add(new Advice(3,Constants.ENGLISH,"Avoid going to crowded places.", "Avoid going to crowded places.",
                 "Where people come together in crowds, you are more likely to come into close contact with someone that has COIVD-19 and it is more difficult to maintain physical distance of 1 metre (3 feet).",
                 R.drawable.ic_wash_hands));
         adviceList.add(new Advice(4,Constants.ENGLISH,"Avoid touching eyes, nose and mouth.","Avoid touching eyes, nose and mouth.",
-                "Hands touch many surfaces and can pick up viruses. Once contaminated, hands can transfer the virus to your eyes, nose or mouth. From there, the virus can enter your body and infect you..",
+                "Hands touch many surfaces and can pick up viruses. Once contaminated, hands can transfer the ic_virus to your eyes, nose or mouth. From there, the ic_virus can enter your body and infect you..",
                 R.drawable.ic_wash_hands));
         adviceList.add(new Advice(5,Constants.ENGLISH,"Make sure you, and the people around you, follow good respiratory hygiene.",
                 "Make sure you, and the people around you, follow good respiratory hygiene. This means covering your mouth and nose with your bent elbow or tissue when you cough or sneeze. Then dispose of the used tissue immediately and wash your hands",
-                "Droplets spread virus. By following good respiratory hygiene, you protect the people around you from viruses such as cold, flu and COVID-19.",
+                "Droplets spread ic_virus. By following good respiratory hygiene, you protect the people around you from viruses such as cold, flu and COVID-19.",
                 R.drawable.ic_wash_hands));
         adviceList.add(new Advice(6,Constants.ENGLISH,"Stay home and self-isolate",
-                "Stay home and self-isolate even with minor symptoms such as cough, headache, mild fever, until you recover. Have someone bring you supplies. If you need to leave your house, wear a mask to avoid infecting others.",
+                "Stay home and self-isolate even with minor symptoms such as cough, headache, mild fever, until you recover. Have someone bring you supplies. If you need to leave your house, wear a ic_prevention to avoid infecting others.",
                 "Avoiding contact with others will protect them from possible COVID-19 and other viruses.",
                 R.drawable.ic_wash_hands));
         adviceList.add(new Advice(7,Constants.ENGLISH,"seek medical attention",
@@ -101,7 +101,7 @@ public class RandomSeeders {
                 " will have it for life.",
                 "You can recover from the coronavirus disease (COVID-19). Catching the new coronavirus DOES NOT mean you" +
                         " will have it for life.",
-                "Most of the people who catch COVID-19 can recover and eliminate the virus from their bodies. If you" +
+                "Most of the people who catch COVID-19 can recover and eliminate the ic_virus from their bodies. If you" +
                         " catch the disease, make sure you treat your symptoms. If you have cough, fever, and difficulty breathing, " +
                         "seek medical care early – but call your health facility by telephone first. Most patients recover thanks to " +
                         "supportive care."));
@@ -111,27 +111,27 @@ public class RandomSeeders {
                         " NOT mean you are free from the coronavirus disease (COVID-19) or any other lung disease.",
                 "The most common symptoms of COVID-19 are dry cough, tiredness and fever. Some people may " +
                         "develop more severe forms of the disease, such as pneumonia. The best way to confirm if you have  the" +
-                        " virus producing COVID-19 disease is with a laboratory test.  You cannot confirm it with this breathing" +
+                        " ic_virus producing COVID-19 disease is with a laboratory test.  You cannot confirm it with this breathing" +
                         " exercise, which can even be dangerous"));
         mythList.add(new Myth(5,Constants.ENGLISH,"Drinking alcohol does not protect you against COVID-19 and can be dangerous",
                 "Drinking alcohol does not protect you against COVID-19 and can be dangerous",
                 "Frequent or excessive alcohol consumption can increase your risk of health problems."));
-        mythList.add(new Myth(6,Constants.ENGLISH,"COVID-19 virus can be transmitted in areas with hot and humid climates",
-                "COVID-19 virus can be transmitted in areas with hot and humid climates",
-                "From the evidence so far, the COVID-19 virus can be transmitted in ALL AREAS, including areas with hot and humid weather. " +
+        mythList.add(new Myth(6,Constants.ENGLISH,"COVID-19 ic_virus can be transmitted in areas with hot and humid climates",
+                "COVID-19 ic_virus can be transmitted in areas with hot and humid climates",
+                "From the evidence so far, the COVID-19 ic_virus can be transmitted in ALL AREAS, including areas with hot and humid weather. " +
                         "Regardless of climate, adopt protective measures if you live in, or travel to an area reporting COVID-19. The best way to protect yourself " +
                         "against COVID-19 is by frequently cleaning your hands. By doing this you eliminate viruses that may be on your hands and avoid infection that " +
                         "could occur by then touching your eyes, mouth, and nose."));
 
         mythList.add(new Myth(7,Constants.CHICHEWA,"chichewa",
-                "COVID-19 virus can be transmitted in areas with hot and humid climates",
-                "From the evidence so far, the COVID-19 virus can be transmitted in ALL AREAS, including areas with hot and humid weather. " +
+                "COVID-19 ic_virus can be transmitted in areas with hot and humid climates",
+                "From the evidence so far, the COVID-19 ic_virus can be transmitted in ALL AREAS, including areas with hot and humid weather. " +
                         "Regardless of climate, adopt protective measures if you live in, or travel to an area reporting COVID-19. The best way to protect yourself " +
                         "against COVID-19 is by frequently cleaning your hands. By doing this you eliminate viruses that may be on your hands and avoid infection that " +
                         "could occur by then touching your eyes, mouth, and nose."));
         mythList.add(new Myth(8,Constants.CHICHEWA,"chichew",
-                "COVID-19 virus can be transmitted in areas with hot and humid climates",
-                "From the evidence so far, the COVID-19 virus can be transmitted in ALL AREAS, including areas with hot and humid weather. " +
+                "COVID-19 ic_virus can be transmitted in areas with hot and humid climates",
+                "From the evidence so far, the COVID-19 ic_virus can be transmitted in ALL AREAS, including areas with hot and humid weather. " +
                         "Regardless of climate, adopt protective measures if you live in, or travel to an area reporting COVID-19. The best way to protect yourself " +
                         "against COVID-19 is by frequently cleaning your hands. By doing this you eliminate viruses that may be on your hands and avoid infection that " +
                         "could occur by then touching your eyes, mouth, and nose."));
